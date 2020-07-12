4513c7ecdde18c40f856fde7d1b44dfe7f60d78c
rinamahure <32775806+rinamahure@users.noreply.github.com>
2020-07-12 11:55:53 +0530

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,10 @@
+<?php
+$conn=mysqli_connect("fdb22.awardspace.net","3489332_credit","Mahure@12345","3489332_credit");
+if(!$conn){
+    echo "Not Connected....";
+}
+else{
+    echo "Connected...";
+}
+
+?>
